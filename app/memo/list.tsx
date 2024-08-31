f54f2e309c4420029792ffbc034ac3476e9eb560
a8121160 <gagamhw333@gmail.com>
2024-09-01 00:40:38 +0900

    アイコンの実装

---
+++ app/memo/list.tsx
@@ -0,0 +1,31 @@
+import { View, Text, StyleSheet } from "react-native"
+import Header from "../../components/Header"
+import MemoListItem from "../../components/MemoListItem"
+import CircleButton from "../../components/CircleButton"
+import Icon from "../../components/Icon"
+
+
+const Index = (): JSX.Element => {
+    return (
+        <View style={styles.container}>
+            <Header />
+            <View>
+                <MemoListItem />
+                <MemoListItem />
+                <MemoListItem />
+            </View>
+            <CircleButton>
+                <Icon name="plus" size={40} color="#ffffff" />
+            </CircleButton>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, //画面いっぱいに要素を広げる
+        backgroundColor: '#ffffff'
+    }
+})
+
+export default Index
